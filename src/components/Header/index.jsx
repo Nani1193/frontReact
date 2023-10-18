20d3f8ba24cf821f033ce7cee569576d54b2ee76
nani1193 <erodriguez1193@gmail.com>
2023-10-18 17:01:43 -0500

    Hay que hacer correcciones

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,6 +1,8 @@
+import styles from './header.module.css';
+
 export function Header(){
     return(
-        <header>
+        <header className='styles'>
             <p>Hola</p>
         </header>
     )
